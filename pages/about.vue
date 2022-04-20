bf6df1ea2a4a921e704f2d74b668b064b86635b2
62160326 <60688879+ZeroNont@users.noreply.github.com>
2022-04-20 16:22:26 +0700

    Header

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="/home">Nuxt</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <nuxt-link to="/home" class="nav-link">หน้าหลัก</nuxt-link>
+                </li>
+                <li class="nav-item">
+                    <nuxt-link to="/posts" class="nav-link">โพสต์</nuxt-link>
+                </li>
+
+            </ul>
+
+        </div>
+    </nav>
+</template>
+<script>
+ export default{
+   layout:"corelayout"
+ }
+</script>
---
+++ pages/home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Home page</h1>
+    <nuxt-link to="/about" class="btn btn-primary">About (internal link that belongs to the Nuxt App)</nuxt-link>
+    <a href="https://nuxtjs.org">External Link to another page</a>
+
+  </div>
+</template>
+<script>
+export default {
+  layout:"corelayout"
+}
+</script>
